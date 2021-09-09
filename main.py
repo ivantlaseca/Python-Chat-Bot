6ce9729899f7262c98edda8520cdeeeb0d18014c
ivantlaseca <57382662+ivantlaseca@users.noreply.github.com>
2021-09-09 07:34:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import random
+import os
+import webbrowser
+
+
 def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
-
-
-def remind_name():
-    print('Please, remind me your name.')
-    name = input()
-    print('What a great name you have, ' + name + '!')
+    print('I was created in ' + birth_year + ' for my creator\'s AI course.')
 
 
 def guess_age():
@@ -18,40 +17,94 @@ def guess_age():
     rem7 = int(input())
     age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
+    print("Your age is " + str(age) + "!")
 
 
-def count():
-    print('Now I will prove to you that I can count to any number you want.')
+def related(request):
+    if "name" in request:
+        understood = "What's your name?"
+    elif "uplift" in request or "cheer" in request:
+        understood = "Uplift me."
+    elif "open" in request:
+        understood = "Open webpage."
+    elif "do" in request and "homework" in request:
+        understood = "Pranked."
+    elif "how" in request and "are" in request and "age" not in request and "old" not in request:
+        understood = "How are you doing?"
+    elif "guess" in request and "my" in request and "age" in request or "old" in request:
+        understood = "How old am I?"
+    else:
+        understood = "Idk"
 
-    num = int(input())
-    curr = 0
-    while curr <= num:
-        print(curr, '!')
-        curr = curr + 1
+    return understood
+
+
+def openWebpage(websiteName):
+    webbrowser.open("http://" + websiteName + ".com", new=1)
+
+
+def uplifts():
+    encouragements = ["You're the best!",
+                      "You've got this!",
+                      "You look handsome today!",
+                      "Don't give up. Suffer now and live the rest of your life as a champion.",
+                      "Prove them wrong.",
+                      "Remember your \"why\". Remember who you do this for.",
+                      "You are a winner. Go out and win it all!"]
+
+    randomEncouragement = random.choice(encouragements)
+
+    print(randomEncouragement)
+
+
+def howAmI():
+    moods = ["I've had better days.",
+             "Having the best day ever!",
+             "Glad to be talking to you.",
+             "Talking to you just made my day a lot better!",
+             "Kinda hungry tbh...",
+             "I'm doing good!.",
+             "Life is amazing it is what it should be."]
+
+    randomMood = random.choice(moods)
+
+    print(randomMood)
+
+
+def functions(name):
+    print("How can I help you today, " + name + "? (\"Exit\" to quit)")
+    request = input()
+    request = request.lower()
+
+    while request != "exit":
+        understood = related(request)
+        if understood == "What's your name?":
+            print("My name is Blex!")
+        elif understood == "Uplift me.":
+            uplifts()
+        elif understood == "Open webpage.":
+            openWebpage(request.split(" ")[1])
+        elif understood == "Pranked.":
+            print("Ha! Good one. Get to work!")
+        elif understood == "How are you doing?":
+            howAmI()
+        elif understood == "How old am I?":
+            guess_age()
+        else:
+            print("Not sure if I can help with that at the moment.")
+        print("How else can I help?")
+        request = input()
+        request = request.lower()
 
 
 def main():
-    print("Let's test your programming knowledge.")
-    print("What language did you just learn?")
-    print("1. Java")
-    print("2. C++")
-    print("3. Python")
-    print("4. C#")
-    answer = int(input())
-    while(answer != 3):
-        print("Sorry, try again.")
-        answer = int(input())
+    greet('Blex', '2021')
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+    functions(name)
 
-    print("Congratulations!")
+    print("Goodbye!")
 
 
-
-
-
-greet('Blex', '2021')  # change it as you need
-remind_name()
-guess_age()
-count()
 main()
-
